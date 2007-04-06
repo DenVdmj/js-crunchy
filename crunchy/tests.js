bf297407f51194905fa50539f8c51c4a1a48f5fe
dnljms <dnljms@98983af7-c629-0410-b196-cb91e6f90569>
2007-04-06 07:21:18 +0000

    Fix a bug when for loop's setup is empty.

--- crunchy/tests.js
+++ crunchy/tests.js
@@ -178,6 +178,7 @@ runTest("var x = {}; x.index = 5", "test(x.index == 5)");
 runTest("var x = '\\\n!'", "test(x === '!')");
 runTest("Number.prototype.flump = 3; var x = 1 .flump;", "test(x === 3)");
 
-/**/
+runTest("var x = 0; for(;;) { if(x === 2) break; ++x; }", "test(x === 2)");
 
+/**/
 print("Finished!");
